ef279f4f5abdf2021ea7fc8c46e7074f92b43a0b
diletantik <shtelmahsergiy@gmail.com>
2017-04-07 17:50:44 +0300

    latest bk changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324113313) do
+ActiveRecord::Schema.define(version: 20170407101434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,9 @@ ActiveRecord::Schema.define(version: 20170324113313) do
     t.string   "provider_id"
     t.string   "provider_name"
     t.boolean  "invitation_sent",        default: false
+    t.string   "role_name"
+    t.string   "role_description"
+    t.string   "role_image_url"
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
