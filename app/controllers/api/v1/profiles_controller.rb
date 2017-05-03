ef5b95aaa28814c31358679d788ed502d818e5a7
diletantik <shtelmahsergiy@gmail.com>
2017-05-03 12:50:06 +0300

    add avatar url to profile

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ProfilesController < ApplicationController
   def show
     user = User.find_by(id: params[:id])
     if user.present?
-      render json: user.as_json, except: [:auth_token, :created_at, :encrypted_password]
+      render json: user.as_json(methods: [:avatar_url]), except: [:auth_token, :created_at, :encrypted_password]
     else
       head 404
     end
